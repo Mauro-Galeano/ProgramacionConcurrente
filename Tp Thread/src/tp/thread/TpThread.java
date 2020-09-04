8297e6251c9d38e3ea9565d4b68e0fb4440a8d8b
carme <carme@192.168.1.70>
2020-09-04 10:25:29 -0300

    main del punto 1

---
+++ Tp Thread/src/tp/thread/TpThread.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp.thread;
+
+/**
+ *
+ * @author carme
+ */
+public class TpThread {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Cliente juan = new Cliente();
+        juan.setName("Juan Lopez");
+        Cliente ines = new Cliente();
+        ines.setName("Ines Garcia");
+        juan.start();
+        ines.start();
+    }
+
+}
