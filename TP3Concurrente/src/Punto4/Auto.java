096ae096a41352fbc097803c1123168dceec584b
carme <carme@192.168.1.70>
2020-09-16 13:25:11 -0300

    TP3 punto 4 final

---
+++ TP3Concurrente/src/Punto4/Auto.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Punto4;
+
+/**
+ *
+ * @author carme
+ */
+public class Auto extends Vehiculo implements Runnable {
+
+    private Surtidor s;
+    private String marca, patente, modelo;
+    private int kmParaService, naftaActual, capacidad;
+
+    public Auto(String pat, String model, String marca, Surtidor st) {
+        this.marca = marca;
+        this.modelo = model;
+        this.patente = pat;
+        this.capacidad=100;
+        this.naftaActual=35;//Random entre 1 y 50.
+        this.s=st;
+    }
+
+    public void andar() {
+        int x=34;
+        try {
+             if((naftaActual-x)>0){
+                naftaActual=naftaActual-x;//Donde x es numero aleatorio
+                 System.out.println("Esta andando el auto "+Thread.currentThread().getName());
+                Thread.sleep(x*100);
+             }
+        } catch (InterruptedException e) {
+
+        }
+
+    }
+
+    public void verificarNafta() {
+        int cargar;
+        if (naftaActual < 10) {
+            cargar=calcularNaftaACargar();
+            s.cargarNafta(cargar);
+        }
+    }
+    
+    public int calcularNaftaACargar(){
+        int x=34;//Este es un numero aleatorio.
+        int resta=capacidad-naftaActual;
+        if(resta>0){
+            naftaActual=naftaActual+x; //x es un numero aleatorio que va a estar entre 0 y el valor de resta
+        }
+        return x;
+    }
+
+    public void run() {
+        while(true){
+            this.andar();
+            this.verificarNafta();
+        }
+    }
+}
---
+++ TP3Concurrente/src/Punto4/Surtidor.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Punto4;
+
+/**
+ *
+ * @author carme
+ */
+public class Surtidor {
+    public Surtidor(){
+        
+    }
+    
+    
+    public synchronized void cargarNafta(int cargar){
+        try{
+            System.out.println("Esta cargando nafta el auto: "+Thread.currentThread().getName()+" esta cargando "+cargar );
+            Thread.sleep(cargar*100);
+            System.out.println("Termino de cargar");
+        }catch(InterruptedException e){
+            
+        }
+        
+    }
+}
