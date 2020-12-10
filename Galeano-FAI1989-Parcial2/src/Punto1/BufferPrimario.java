b99d43a79830dfb7ecdff8b785884f81c5b37b66
carme <carme@192.168.1.70>
2020-12-10 18:20:57 -0300

    Punto 1 tema 2 con locks

--- Galeano-FAI1989-Parcial2/src/Punto1/BufferPrimario.java
+++ Galeano-FAI1989-Parcial2/src/Punto1/BufferPrimario.java
@@ -18,47 +18,54 @@ public class BufferPrimario {
     private int capacidadMaxima, capacidadActual;
     private Cola cola= new Cola();//para darle orden
     private char[] peticionesPrimaria;
-    private Lock lockEscrituraP,lockLectura;//,lockEscrituraS;
-    private Condition lectura;
+    private Lock lockBufferP,lockBufferS,lockCola;
+    private Condition espera;
+    boolean bandera=false;//se escribio en el primario?
     public BufferPrimario(int c) {
         capacidadMaxima = c;//capacidad maxima del buffer primario
         capacidadActual = 0;
         peticionesPrimaria = new char[c];
-        lockEscrituraP=new ReentrantLock();//lock para asegurar exclusion mutua a la hora de escribir en el buffer primario
-        //lockEscrituraS=new ReentrantLock();//lock para asegurar exclusion mutua a la hora de escribir en el buffer secundario
-        lockLectura=new ReentrantLock();
-        lectura= lockLectura.newCondition();
+        lockBufferP=new ReentrantLock();//lock para asegurar exclusion mutua a la hora de escribir en el buffer primario
+        lockBufferS=new ReentrantLock();//lock para asegurar exclusion mutua a la hora de escribir en el buffer secundario
+        lockCola=new ReentrantLock();
+        espera=lockCola.newCondition();
     }
     //lock solo para cada buffer. tanto para escribir como para leer, y uno general para la cola
     public void escribir(char aEscribir) {
-        boolean bandera=false;
-        lockEscrituraP.lock();
-        if (capacidadMaxima <= capacidadActual) {//puedo usar tryLock()??
-            System.out.println("Soy " + Thread.currentThread().getName() + " el buffer primario esta lleno, se guarda " + aEscribir + " en el secundario");
-            //liberar escritura primaria. y usar escritura secundaria
+        //boolean bandera=false;//se escribio en el primario?
+        lockBufferP.lock();
+        if (capacidadMaxima <= capacidadActual) {
+            System.out.println("Soy " + Thread.currentThread().getName() + " el buffer primario esta lleno, se guardará " + aEscribir + " en el secundario");
+            lockBufferP.unlock();//liberar escritura primaria. y usar escritura secundaria
             this.escribirEnSecundaria(aEscribir);
         } else {
+            lockCola.lock();
             cola.poner(aEscribir);
-            System.out.println(cola.toString());
+            espera.signalAll();//aviso que se agrego algo al buffer para que el servidor imprima
+            lockCola.unlock();
             peticionesPrimaria[capacidadActual] = aEscribir;
             capacidadActual++;
             System.out.println("Soy " + Thread.currentThread().getName() + " escribi " + aEscribir + " en el primario");
-            bandera=true;
-            lockEscrituraP.unlock();
+            lockBufferP.unlock();//liberar escritura primaria. y usar escritura secundaria
+            //bandera=true;
+            //lockEscrituraP.unlock();
         }
-        lockLectura.lock();
-        lectura.signal();//aviso al servidor el cual esta dormido que puede imprimir
-        lockLectura.unlock();
-        if(!bandera){
-            lockEscrituraP.unlock();
-        }
+//        lockLectura.lock();
+//        lectura.signal();//aviso al servidor el cual esta dormido que puede imprimir
+//        lockLectura.unlock();
+//        if(bandera){
+//            lockBufferP.unlock();
+//        }
     }
 
     private void escribirEnSecundaria(char aEscribir) {
-        //lockEscrituraS.lock();
+        lockBufferS.lock();
+        lockCola.lock();
         cola.poner(aEscribir);//guardo en la cola para despues imprimir
+        espera.signalAll();//aviso que se agrego algo al buffer para que el servidor imprima
+        lockCola.unlock();
         System.out.println("Soy " + Thread.currentThread().getName() + " escribi " + aEscribir + " en el secundario");
-        //lockEscrituraS.unlock();
+        lockBufferS.unlock();
     }
 
     private boolean verificarSiEstaEnPrimario(char a) {//sirve para verificar si el elemento que imprimi esta en el buffer primario
@@ -75,20 +82,20 @@ public class BufferPrimario {
     }
 
     public void imprimir() throws InterruptedException {//se dormiria sobre el lock de la cola, si esta vacia se duerme sobre el buffer de la cola.
-        lockLectura.lock();
+        lockCola.lock();
         while (cola.esVacia()) {//verifico si hay algo en la cola para imprimir
             System.out.println("Soy " + Thread.currentThread().getName() + " no puedo imprimir, no hay nada en los buffers");
-            //agergar sleep para simular escritura
-            lectura.await();
+            espera.await();//duermo al servidor hasta que agreguen algo para imprimir
         }
         char x = (char) cola.obtenerFrente();
+        lockBufferP.lock();
         if (this.verificarSiEstaEnPrimario(x)) {//verifico si esta en el buffer primario, para saber si debo disminuir la cantidad actual del buffer limitado
             capacidadActual--;
         }
-        System.out.println("Soy " + Thread.currentThread().getName() + " voy a imprimir");
-        System.out.println(cola.obtenerFrente());
+        lockBufferP.unlock();
+        System.out.println("Soy " + Thread.currentThread().getName() + " voy a imprimir "+cola.obtenerFrente());
+        //System.out.println(cola.obtenerFrente());
         cola.sacar();//saco lo que imprimi
-        lockLectura.unlock();
+        lockCola.unlock();
     }
-
 }
